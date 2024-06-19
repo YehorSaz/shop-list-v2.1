40b784118841ea9d3426c8e39387091d27d64585
YehorSaz <inercoin@gmal.com>
2024-06-19 13:00:37 +0300

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import {Footer, Header, ShopList} from "./components";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
 
     const [trigger, setTrigger] = useState(false);
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        })
+    }, []);
 
 
   return (
