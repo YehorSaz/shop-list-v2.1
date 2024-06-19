c469f2bb3198edb13cf0e133ac8c6a191f1296bc
YehorSaz <inercoin@gmal.com>
2024-06-19 12:42:02 +0300

    edit app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
 
   return (
-    <div className="App"}>
+    <div className={"App"}>
         <Header/>
         <ShopList/>
         <Footer trigger={trigger} setTrigger={setTrigger}/>
